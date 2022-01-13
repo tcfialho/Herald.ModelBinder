855c40e1ef809a63a128d708109761581e998b9c
Thiago F <tcfialho@gmail.com>
2022-01-13 03:36:44 -0300

    feat: model binder

---
+++ Herald.ModelBinder/RouteAndBody/RouteAndBodyeModelBinderProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Herald.ModelBinder.RouteAndBody
+{
+    public class RouteAndBodyModelBinderProvider : IModelBinderProvider
+    {
+        private readonly BodyModelBinderProvider _bodyModelBinderProvider;
+        private readonly ComplexTypeModelBinderProvider _complexTypeModelBinderProvider;
+
+        public RouteAndBodyModelBinderProvider(BodyModelBinderProvider bodyModelBinderProvider,
+            ComplexTypeModelBinderProvider complexObjectModelBinderProvider)
+        {
+            _bodyModelBinderProvider = bodyModelBinderProvider;
+            _complexTypeModelBinderProvider = complexObjectModelBinderProvider;
+        }
+
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            var bodyBinder = _bodyModelBinderProvider.GetBinder(context);
+            var complexBinder = _complexTypeModelBinderProvider.GetBinder(context);
+
+            return context.BindingInfo.BindingSource != null
+                   && context.BindingInfo.BindingSource.CanAcceptDataFrom(RouteAndBodyBindingSource.RouteAndBody)
+                   && bodyBinder != null
+                   && complexBinder != null
+                ? new RouteAndBodyModelBinder(bodyBinder, complexBinder)
+                : default(IModelBinder);
+        }
+    }
+}
---
+++ Herald.ModelBinder/RouteAndQuery/FromRouteAndQueryAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+using System;
+
+namespace Herald.ModelBinder.RouteAndQuery
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter, Inherited = true, AllowMultiple = false)]
+	public class FromRouteAndQueryAttribute : Attribute, IBindingSourceMetadata
+	{
+		public BindingSource BindingSource { get; } = CompositeBindingSource.Create(
+			new[] { BindingSource.Path, BindingSource.Query }, nameof(FromRouteAndQueryAttribute));
+	}
+}
