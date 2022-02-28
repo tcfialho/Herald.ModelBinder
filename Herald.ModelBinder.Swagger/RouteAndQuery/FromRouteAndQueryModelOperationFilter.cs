26d97b75fa294a7b5e1f6f815769fe1c6c52839f
Thiago F <tcfialho@gmail.com>
2022-02-28 17:11:28 -0300

    fix: swagger for path and route

--- Herald.ModelBinder.Swagger/RouteAndQuery/FromRouteAndQueryModelOperationFilter.cs
+++ Herald.ModelBinder.Swagger/RouteAndQuery/FromRouteAndQueryModelOperationFilter.cs
@@ -13,16 +13,24 @@ namespace Herald.ModelBinder.Swagger
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
             var parameterDescriptions = context.ApiDescription.ParameterDescriptions
-                .Where(x=>x.Source.DisplayName == nameof(FromRouteAndQueryAttribute))
+                .Where(x => x.Source.DisplayName == nameof(FromRouteAndQueryAttribute))
                 .Join(context.ApiDescription.ParameterDescriptions, c => c.Name.ToUpper(), c2 => c2.Name.ToUpper(), (c, c2) => new { c, c2 })
                 .Where(x => x.c != x.c2)
                 .Select(x => x.c)
                 .ToArray();
-             
+
             foreach (var parameterDescription in parameterDescriptions)
             {
-                context.ApiDescription.ParameterDescriptions.Remove(parameterDescription);
-                operation.Parameters.Remove(operation.Parameters.Single(p => p.Name == parameterDescription.Name));
+                if (context.ApiDescription.ParameterDescriptions.Contains(parameterDescription))
+                {
+                    context.ApiDescription.ParameterDescriptions.Remove(parameterDescription);
+                }
+
+                var parameter = operation.Parameters.FirstOrDefault(p => p.Name == parameterDescription.Name);
+                if (parameter != null)
+                {
+                    operation.Parameters.Remove(parameter);
+                }
             }
         }
     }
